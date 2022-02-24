6fa680438c7a51e19c16b24c5f89d0b4ef6d40b7
Ozan Gürer <ozangurer12@gmail.com>
2022-02-24 21:45:16 +0300

    Register a User with hashed password.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-router.get("/", (req, res) => {
-    res.send('Auth routes')
+//Register
+router.post("/register", async (req, res) => {
+    try {
+        // Hashing the password.
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        // Register a new user.
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        // Saving user, returning response.
+        const user = await newUser.save()
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err)
+    }
 })
+
+//Login
 
 module.exports = router;
