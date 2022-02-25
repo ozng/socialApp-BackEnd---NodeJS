b729c678001fe6a8c1a2c8efb26643d6b025ad33
Ozan Gürer <ozangurer12@gmail.com>
2022-02-25 14:03:57 +0300

    Follow - Unfollow user

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.put("/:id", async (req, res) => {
                 const salt = await bcrypt.genSalt(10)
                 req.body.password = await bcrypt.hash(req.body.password, salt)
             } catch (err) {
-                return res.status(500).json("Something went wrong with updating password.")
+                return res.status(500).json("Something went wrong with updating your password.")
             }
         }
         try {
@@ -20,7 +20,7 @@ router.put("/:id", async (req, res) => {
             })
             res.status(200).json("Profile is updated.")
         } catch (err) {
-            return res.status(500).json(err)
+            return res.status(500).json("Something went wrong with updating your profile")
         }
     } else {
         return res.status(403).json("You have no authorization to update this user.")
@@ -34,7 +34,7 @@ router.delete("/:id", async (req, res) => {
             const user = await User.deleteOne({ _id: req.body.userId })
             res.status(200).json("Profile is deleted.")
         } catch (err) {
-            return res.status(500).json("Something went wrong.")
+            return res.status(500).json("Something went wrong with deleting.")
         }
     } else {
         return res.status(403).json("You have no authorization to delete this user.")
@@ -51,6 +51,44 @@ router.get('/:id', async (req, res) => {
     }
 })
 // Follow User
+router.put("/:id/follow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id)
+            const currentUser = await User.findById(req.body.userId)
+            if (!user.follower.includes(req.body.userId)) {
+                await user.updateOne({ $push: { follower: req.body.userId } })
+                await currentUser.updateOne({ $push: { following: req.params.id } })
+                res.status(200).json(`You have follow ${user.username}.`)
+            } else {
+                res.status(403).json('You already follow this user.')
+            }
+        } catch (err) {
+            res.status(500).json("Something went wrong.")
+        }
+    } else {
+        res.status(403).json("You cannot follow yourself.")
+    }
+})
 // Unfollow User
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id)
+            const currentUser = await User.findById(req.body.userId)
+            if (user.follower.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { follower: req.body.userId } })
+                await currentUser.updateOne({ $pull: { following: req.params.id } })
+                res.status(200).json(`You have unfollow ${user.username}.`)
+            } else {
+                res.status(403).json('You already not follow this user.')
+            }
+        } catch (err) {
+            res.status(500).json("Something went wrong.")
+        }
+    } else {
+        res.status(403).json("You cannot unfollow yourself.")
+    }
+})
 
 module.exports = router;
