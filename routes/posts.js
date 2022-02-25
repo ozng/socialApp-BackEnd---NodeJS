ee129b90650e9d56dca5ac675137e6d8925c9e72
Ozan Gürer <ozangurer12@gmail.com>
2022-02-25 16:55:56 +0300

    Like-Unlike-Get a Post and Feed

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require('../models/Post')
+const User = require('../models/User')
 
 // Create post.
 
@@ -42,9 +43,50 @@ router.delete("/:id", async (req, res) => {
         res.status(500).json("Something went wrong with deleting a post.")
     }
 })
-// Like post.
+// Like and unlike post.
+
+router.put('/:id/like', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post.like.includes(req.body.userId)) {
+            await post.updateOne({ $push: { like: req.body.userId } })
+            res.status(200).json("You have been like this post")
+        } else {
+            await post.updateOne({ $pull: { like: req.body.userId } })
+            res.status(200).json("You have been unlike this post")
+        }
+
+    } catch (err) {
+        res.status(500).json("Something went wrong.")
+    }
+
+})
 // Get a post.
+
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        res.status(200).json(post)
+    } catch (err) {
+        res.status(500).json("Something went wrong.")
+    }
+})
 // Get a feed.
+
+router.get("/feed/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId)
+        const userPosts = await Post.find({ userId: currentUser._id })
+        const friendPosts = await Promise.all(
+            currentUser.following.map(friendId => {
+                return Post.find({ userId: friendId })
+            })
+        )
+        res.status(200).json(userPosts.concat(...friendPosts))
+    } catch (err) {
+        res.status(500).json("Something went wrong.")
+    }
+})
 
 
 module.exports = router;
