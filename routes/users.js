01c1b90ead003c2a85d60fc03787516bef2cdfe7
Ozan Gürer <ozangurer12@gmail.com>
2022-03-01 17:38:26 +0300

    fixed get a user

--- routes/users.js
+++ routes/users.js
@@ -46,7 +46,7 @@ router.get('/', async (req, res) => {
     const username = req.query.username;
 
     try {
-        const user = userId ? await User.findById(req.params.id) : await User.findOne({ username: username })
+        const user = userId ? await User.findById(userId) : await User.findOne({ username: username })
         const { password, updatedAt, ...other } = user._doc
         res.status(200).json(other)
     } catch (err) {
